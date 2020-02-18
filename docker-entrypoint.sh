370bcc557ea4c8a45d2867dbf9bbc4cbb9a31e56
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-02-17 22:06:59 -0300

    updeate  code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-set -e
-
-bundle install --jobs 20 --retry 5
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
-
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+bundle update --jobs 20 --retry 5
