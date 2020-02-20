04bb1aee2b40c205babb39faa5b0e60685339f31
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-02-19 22:42:02 -0300

    intall ruby

---
+++ web/bomcupom/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation';
+import Form from './components/Form';
+import './css/styles.css'
+
+function App() {
+  return (
+  <>
+  <Navigation></Navigation>
+  <Form></Form>
+  </>
+  );
+}
+
+export default App;
